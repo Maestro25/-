c0d4c027de83054bbfc6363a880d9dbcc1ef89cf
Maestro25 <73071109+Maestro25@users.noreply.github.com>
2023-07-14 11:45:43 +0300

    Add files via upload

---
+++ feedback/js/tel-mask.js
@@ -0,0 +1,8 @@
+// masked_input_1.4.1-min.js
+// angelwatt.com/coding/masked_input.php
+
+MaskedInput({
+    elm: document.getElementById('tel'), // select by id
+    format: '(___)___-__-__',
+    separator: '()-'
+  });
